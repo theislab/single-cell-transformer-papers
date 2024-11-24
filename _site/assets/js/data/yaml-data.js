7273ee971af85057a9fc9e9bf518468584b19947
Paul O.-P <paulo.-p.@Pauls-MacBook-Pro.local>
2024-11-24 18:31:56 +0100

    Remove _site directory from git tracking

--- _site/assets/js/data/yaml-data.js
+++
@@ -1,3 +0,0 @@
-window.singleCellTransformers = {{ site.data.single-cell-transformers | jsonify }};
-window.transformerLLMs = {{ site.data.transformer-llms | jsonify }};
-window.transformerEvaluation = {{ site.data.transformer-evaluation | jsonify }}; 
